56d8eae4559925e7b938ea03e06e1f20f3440642
ZhengPeterWang <peterwz@stanford.edu>
2024-01-31 22:02:19 -0800

    New training test cases

--- tests/unit_tests/InterventionUtilsTestCase.py
+++ tests/unit_tests/InterventionUtilsTestCase.py
@@ -1,4 +1,5 @@
 import unittest
+import torch.nn.functional as F
 from ..utils import *
 from pyvene.models.intervention_utils import _do_intervention_by_swap
 from pyvene.models.interventions import VanillaIntervention
@@ -174,9 +175,26 @@ class InterventionUtilsTestCase(unittest.TestCase):
         golden = torch.tensor([[3, 4, 5, 6, 7, 8], [9, 10, 11, 12, 13, 14],])
         self.assertTrue(torch.allclose(golden, output))
 
-    def test_low_rank_rotated_space_positive(self):
-        # TODO: implement
-        pass
+    def test_brs_gradient_positive(self):
+        intervention = BoundlessRotatedSpaceIntervention(embed_dim=6)
+        intervention.temperature = torch.nn.Parameter(torch.tensor(2.0))
+        base = torch.arange(12).float().view(2, 6)
+        source = torch.arange(12, 24).float().view(2, 6)
+        
+        optimizer_params = []
+        optimizer_params += [{"params": intervention.rotate_layer.parameters()}]
+        optimizer_params += [{"params": intervention.intervention_boundaries}]
+        optimizer = torch.optim.Adam(optimizer_params, lr=1e-1)
+
+        golden = torch.tensor([[5, 6, 7, 8, 9, 10], [11, 12, 13, 14, 15, 16],]).float()
+
+        for _ in range(1000):
+            optimizer.zero_grad()
+            output = intervention(base, source)
+            loss = F.mse_loss(output, golden)
+            loss.backward()
+            optimizer.step()
+        self.assertTrue(torch.allclose(golden, output,rtol=1e-02, atol=1e-02))
 
 
 def suite():
@@ -206,6 +224,7 @@ def suite():
     suite.addTest(InterventionUtilsTestCase("test_collect_intervention_positive"))
     suite.addTest(InterventionUtilsTestCase("test_collect_intervention_negative"))
     suite.addTest(InterventionUtilsTestCase("test_brs_intervention_positive"))
+    suite.addTest(InterventionUtilsTestCase("test_brs_gradient_positive"))
 
     # TODO: Add tests to other interventions
     return suite
