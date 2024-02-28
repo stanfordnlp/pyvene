e10b321bb460484310a616f2196a1b8d1c2cf40a
frankaging <zxw0424@gmail.com>
2024-02-27 20:45:38 -0800

    minor adjust on test case

--- tests/integration_tests/InterventionWithMLPTestCase.py
+++ tests/integration_tests/InterventionWithMLPTestCase.py
@@ -112,7 +112,8 @@ class InterventionWithMLPTestCase(unittest.TestCase):
         )
         base = {"inputs_embeds": torch.rand(10, 1, 3)}
         self.assertTrue(
-            torch.allclose(ONE_MLP_CLEAN_RUN(base, self.mlp), intervenable(base)[0][0])
+            torch.allclose(ONE_MLP_CLEAN_RUN(base, self.mlp), intervenable(
+                base, output_original_output=True)[0][0])
         )
 
     def test_with_subspace_positive(self):
