589d0a5afc0eda57f60d763e399893960a20b966
Aryaman Arora <aryaman.arora2020@gmail.com>
2024-07-13 14:54:24 -0700

    lol i didnt import pyvene

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,7 +5,9 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../pyvene'))
+sys.path.insert(0, os.path.abspath('../..'))
+print(sys.path)
+import pyvene
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
