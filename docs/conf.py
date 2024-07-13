37d3c519317348d0a68b0c82ebda082a8c2398a4
Aryaman Arora <aryaman.arora2020@gmail.com>
2024-07-13 14:31:23 -0700

    attempt to fix pyvene ref

--- docs/conf.py
+++ docs/conf.py
@@ -5,7 +5,7 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../pyvene'))
+sys.path.insert('../pyvene')
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
